d702ed15e333f88d308f65eab5088c1a6f33ee7f
satriaamien <satriaamien0@gmail.com>
2023-10-04 15:42:17 +0700

    done

--- challenge.js
+++ challenge.js
@@ -3,11 +3,13 @@ import {
   describe,
   expect,
 } from "https://jslib.k6.io/k6chaijs/4.3.4.3/index.js";
-import { Rate } from "k6/metrics";
+import { Gauge, Rate } from "k6/metrics";
 
 const completedRateAge = new Rate("test_api_completed_rate_age");
 const completedRatestatus = new Rate("test_api_completed_rate_status");
 const completedRatestatus_yt = new Rate("test_api_completed_rate_status_yt");
+const GaugeContentSize = new Gauge("ContentSize");
+const allErrors = new Counter("error_counter");
 
 export default function () {
   describe("check test api", () => {
@@ -15,16 +17,17 @@ export default function () {
       const res = http.get(`https://test-api.k6.io/public/crocodiles/${i}/`, {
         tags: { judul: "test-api-crocodile" },
       });
+      GaugeContentSize.add(res.body.length);
       completedRateAge.add(res.json().age >= 20);
       completedRatestatus.add(res.status === 200);
       expect(res.status, "status 200? ").equal(200);
     }
   });
-  describe("check test api", () => {
-    const res = http.get(`https://youtube.com`, {
-      tags: { judul: "test-api-crocodile" },
-    });
-    completedRatestatus_yt.add(res.status === 200);
-    expect(res.status, "status 200? ").equal(200);
-  });
+  //   describe("check test api 2", () => {
+  //     const res = http.get(`https://www.youtube.com`, {
+  //       tags: { judul: "test-api-crocodile" },
+  //     });
+  //     completedRatestatus_yt.add(res.status === 200);
+  //     expect(res.status, "status 200? ").equal(200);
+  //   });
 }
